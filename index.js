72a97f23cd32dab64dc2a303cf90dcf3ce1ed353
NeYziX <38168101+NeYziX@users.noreply.github.com>
2018-04-08 14:34:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,36 +12,6 @@ bot.login("NDMyMjA1ODMzNzIwNDMwNjEy.DaqD3A.T9soBVTbtefWwZOFR3U6zb6bUi0");
 
 
 bot.on('message', message => {
-    let command = message.content.split(" ")[0];
-    const args = message.content.slice(prefix.length).split(/ +/);
-    command = args.shift().toLowerCase();
-
-    if (command === "kick") {
-        let modRole = message.guild.roles.find("name", "Fondateurs");
-        if(!message.member.roles.has(modRole.id)) {
-            return message.reply("Erreur : Permissions insuffisantes.").catch(console.error);           
-        }
-        if(message.mentions.users.size === 0) {
-            return message.reply("Erreur : Mentionne la personne à expluser.").catch(console.error);
-        }
-        let kickMember = message.guild.member(message.mentions.users.first());
-        if(!kickMember) {
-            return message.reply("Cet utilisateur est introuvable ou impossible à expulser.")
-        }
-        if(!message.guild.member(bot.user).hasPermission("KICK_MEMBERS")) {
-            return message.reply("Je n'ai pas les permissions pour faire ceci.").catch(console.error);
-        }
-        kickMember.kick().then(member => {
-            message.reply(`${member.user.username} a été expulsé avec succès.`).catch(console.error);
-            message.guild.channels.find("name", "general").send(`**${member.user.username}** a été kick par **${message.author.username}.**`)
-        }).catch(console.error)
-
-    }
-})
-
-
-
-bot.on('message', message => {
     if (message.content === prefix + "help"){
         message.channel.sendMessage("```⇩Liste des commandes⇩: \n &help \n &site \n &invite```");
         message.delete();
