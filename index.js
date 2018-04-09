982e8ff1882a5ec1e2886ac82cfab069cdf7b5ad
NeYziX <38168101+NeYziX@users.noreply.github.com>
2018-04-09 21:22:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client();
 var prefix = ("&");
 
 bot.on("ready", function() {
-    bot.user.setGame("Aide | &help");
+    bot.user.setGame("Aide | &help | by NeYziX");
     console.log("Connecté avec succès")
 });
 
