187bf44b680c8b6d0cf20c47e321df2d891b0b30
NeYziX <38168101+NeYziX@users.noreply.github.com>
2018-04-10 21:47:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ bot.on("ready", function() {
   console.log("le bot a démarré"); 
 });
 
-bot.login("NDMzMzUwOTAzMzQ1NTc3OTk0.Da6lHw.qUyAtprGVzju5SUOgpxyQgNvvf4");
+bot.login("NDMzMzUwOTAzMzQ1NTc3OTk0.Da6mIQ.fbL4rUxPnOEGdS9fEevgKb39wOA");
 
 bot.on('message', message => {
 if (message.content === prefix + "invite") {
