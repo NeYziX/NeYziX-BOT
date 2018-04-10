32050a4d5d8b1bc245412331da06b322ed7642d2
NeYziX <38168101+NeYziX@users.noreply.github.com>
2018-04-10 19:54:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const bot = new Discord.Client();
 var prefix = ("&");
 
 bot.on("ready", function() {
-  bot.user.setActivity(`dans ${client.guilds.size} serveurs`)
-  console.log(`le bot a démarré avec,  ${client.users.size} personnes, dans ${client.guilds.size} serveurs `); 
+  bot.user.setGame("dans ${client.guilds.size} serveurs")
+  console.log("le bot a démarré avec,  ${client.users.size} personnes, dans ${client.guilds.size} serveurs"); 
 });
 
 bot.login("NDMyOTc4MzcxMDc3NDcyMjU3.Da1KDg.GuaCfKr5JhXz2RjL4z4QPVRcTpU");
