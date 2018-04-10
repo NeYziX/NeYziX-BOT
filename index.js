52c6994d5349d6f47a08cbf5479a63899bf6ece7
NeYziX <38168101+NeYziX@users.noreply.github.com>
2018-04-10 19:45:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var prefix = ("&");
 client.on("ready", () => {
   
   console.log(`le bot a démarré avec,  ${client.users.size} personnes, dans ${client.guilds.size} serveurs `); 
-  client.user.setActivity(`dans ${client.guilds.size} serveurs`)
+  bot.user.setActivity(`dans ${client.guilds.size} serveurs`)
 });
 
 bot.login("NDMyOTc4MzcxMDc3NDcyMjU3.Da1KDg.GuaCfKr5JhXz2RjL4z4QPVRcTpU");
